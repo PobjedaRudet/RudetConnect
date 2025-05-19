be621af8e59ace23798c2952d6098195257a9a4a
Ahmet Hadžiahmetović <info@pobjeda.com>
2025-05-19 13:24:34 +0200

    Brisanje demo1

--- demo1/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace demo1;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
